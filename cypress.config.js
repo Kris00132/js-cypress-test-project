dc02968e684a5d661dfd5de1137f139eacf15601
kristina-yanchukova <k.yanchukova@a1qa.com>
2026-01-08 09:46:18 +0100

    update gitirgore

--- cypress.config.js
+++ cypress.config.js
@@ -5,18 +5,20 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
-
       return config;
     },
-    baseUrl: "https://example.cypress.io", // change if you want
+
+    baseUrl: "https://example.cypress.io",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "cypress/support/e2e.js",
     screenshotOnRunFailure: true,
     video: false
   },
+
   env: {
     allure: true,
     allureResultsPath: "allure-results",
-    allureReuseAfterSpec: true
+    allureReuseAfterSpec: true,
+    allureSkipAutomaticScreenshots: false
   }
 });
